6472e85f8d04e020a1e5e9948532972f14822f78
ADIEL LARA LEDESMA <17171394@itculiacan.edu.mx>
2021-12-15 21:29:08 -0700

    Terminado

--- src/main/java/com/residencias/proyecto/dto/Participante.java
+++ src/main/java/com/residencias/proyecto/dto/Participante.java
@@ -6,7 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.List;
 
@@ -22,8 +25,6 @@ public class Participante {
     private Integer id;
 
     private String nombreCompleto;
-
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd/MM/yyyy HH:mm:ss", timezone = "GMT+8")
     private Date horaUnion;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd/MM/yyyy HH:mm:ss", timezone = "GMT+8")
@@ -33,10 +34,7 @@ public class Participante {
     private String rol;
 
     private boolean participaciónValida;
-
-    //@ManyToOne
-    //@JoinColumn(name="reunion_id", nullable=false)
-    //private Reunion reunion;
+    private boolean asistencia;
 
 
 }
--- src/main/java/com/residencias/proyecto/rabbitmq/MessageListener.java
+++ src/main/java/com/residencias/proyecto/rabbitmq/MessageListener.java
@@ -1,11 +1,15 @@
 package com.residencias.proyecto.rabbitmq;
 
+import com.residencias.proyecto.dto.Participante;
 import com.residencias.proyecto.dto.ReunionStatus;
 import com.residencias.proyecto.rabbitmq.config.MessagingConfig;
 import com.residencias.proyecto.repository.ReunionRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MessageListener {
@@ -17,6 +21,17 @@ public class MessageListener {
     public void listener(ReunionStatus message){
 
         System.out.println(message);
+
+        List<Participante> participantes = new ArrayList();
+
+        message.getReunion().getParticipantes().forEach( (element) -> {
+            if(element.isAsistencia())
+                participantes.add(element);
+        });
+
+        message.getReunion().setParticipantes(participantes);
+
+
         repository.save(message.getReunion());
     }
 
